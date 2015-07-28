336570bf8fdbdf4887023d3e26d761a198f477ef
unknown <danieltiefenauer@DRAGORAN.bbv.intern>
2015-07-28 10:50:06 +0200

    added some more columns

--- FumettoJPA/src/main/java/ch/bbv/fumetto/model/AgendaItem.java
+++ FumettoJPA/src/main/java/ch/bbv/fumetto/model/AgendaItem.java
@@ -9,79 +9,162 @@ import javax.persistence.GenerationType;
 import javax.persistence.Column;
 import javax.persistence.Version;
 import java.lang.Override;
+import java.sql.Timestamp;
+
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @Table(name = "AGENDA_ITEM")
 @XmlRootElement
-public class AgendaItem implements Serializable
-{
+public class AgendaItem implements Serializable {
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.AUTO)
-   @Column(name = "id", updatable = false, nullable = false)
-   private Long id;
-   @Version
-   @Column(name = "version")
-   private int version;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3728801329064894614L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id;
+	@Version
+	@Column(name = "version")
+	private int version;
+	
+	@Column(name = "detail")
+	private String detail;
+	
+	@Column(name = "category")
+	private String category;
+	
+	@Column(name = "location")
+	private String location;
+	
+	@Column(name = "subject")
+	private String subject;
+	
+	@Column(name = "location_detail")
+	private String locationDetail;
+	
+	@Column(name = "time_start")
+	private String timeStart;
+	
+	@Column(name = "time_end")
+	private String timeEnd;
+	
+	@Column(name = "agenda_date")
+	private Timestamp date;
 
-   public Long getId()
-   {
-      return this.id;
-   }
+	public Long getId() {
+		return this.id;
+	}
 
-   public void setId(final Long id)
-   {
-      this.id = id;
-   }
+	public void setId(final Long id) {
+		this.id = id;
+	}
 
-   public int getVersion()
-   {
-      return this.version;
-   }
+	public int getVersion() {
+		return this.version;
+	}
 
-   public void setVersion(final int version)
-   {
-      this.version = version;
-   }
+	public void setVersion(final int version) {
+		this.version = version;
+	}
 
-   @Override
-   public String toString()
-   {
-      String result = getClass().getSimpleName() + " ";
-      if (id != null)
-         result += "id: " + id;
-      return result;
-   }
+	@Override
+	public String toString() {
+		String result = getClass().getSimpleName() + " ";
+		if (id != null)
+			result += "id: " + id;
+		return result;
+	}
 
-   @Override
-   public boolean equals(Object obj)
-   {
-      if (this == obj)
-      {
-         return true;
-      }
-      if (!(obj instanceof AgendaItem))
-      {
-         return false;
-      }
-      AgendaItem other = (AgendaItem) obj;
-      if (id != null)
-      {
-         if (!id.equals(other.id))
-         {
-            return false;
-         }
-      }
-      return true;
-   }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof AgendaItem)) {
+			return false;
+		}
+		AgendaItem other = (AgendaItem) obj;
+		if (id != null) {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-   @Override
-   public int hashCode()
-   {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + ((id == null) ? 0 : id.hashCode());
-      return result;
-   }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getLocationDetail() {
+		return locationDetail;
+	}
+
+	public void setLocationDetail(String locationDetail) {
+		this.locationDetail = locationDetail;
+	}
+
+	public String getTimeStart() {
+		return timeStart;
+	}
+
+	public void setTimeStart(String timeStart) {
+		this.timeStart = timeStart;
+	}
+
+	public String getTimeEnd() {
+		return timeEnd;
+	}
+
+	public void setTimeEnd(String timeEnd) {
+		this.timeEnd = timeEnd;
+	}
+
+	public Timestamp getDate() {
+		return date;
+	}
+
+	public void setDate(Timestamp date) {
+		this.date = date;
+	}
 }
